0440daf098a6126abe1bda06627058403a2806e8
rafaelmt35 <99629720+rafaelmt35@users.noreply.github.com>
2022-03-29 10:15:06 +0700

    first commit

---
+++ Car_API/api.py
@@ -0,0 +1,109 @@
+from Car_API import db_call as db
+import json
+import os
+
+
+class IntelligenceSystemCar:
+    def ask_question(self):
+        cwd = os.getcwd()
+        with open(cwd+"/Car_API/question_answer.json") as qaFile:
+            q_a = json.load(qaFile)
+            qaFile.close()
+
+        print("Tips!!!")
+        print(q_a["tips"])
+
+        # Start to ask question
+        question_number = 1
+        for i in range(9):
+            # If user didn't pick "Petrol" or "Diesel" for the fuel type,
+            # then don't need to ask for the transmission type.
+            # Because all "Electric" car always use "Direct drive" transmission type,
+            # and all "Hybird" car always use either "Automatic" or "Automated manual"
+            # transmission type.
+            if i == 3:
+                if "Petrol" in self.user_want["fuel_type"]  or "Diesel" in self.user_want["fuel_type"] or "Any" in self.user_want["fuel_type"]:
+                    question = f"q{i+1}"
+                else:
+                    if "Electric" in self.user_want["fuel_type"] and "Hybrid" in self.user_want["fuel_type"]:
+                        self.user_want["transmission_type"] = [
+                            "Direct drive", "Automatic", "Automated manual"]
+                    elif "Electric" in self.user_want["fuel_type"] and "Hybrid" not in self.user_want["fuel_type"]:
+                        self.user_want["transmission_type"] = ["Direct drive"]
+                    elif "Electric" not in self.user_want["fuel_type"] and "Hybrid" in self.user_want["fuel_type"]:
+                        self.user_want["transmission_type"] = [
+                            "Automatic", "Automated manual"]   
+                    continue
+            else:
+                question = f"q{i+1}"
+
+            #Print the question
+            print(f"\nQuestion {question_number}")
+            print(q_a[question]["q"])
+
+            # Print the answer and ask for input
+            # If this is first question which is about min and max price we get 2 input
+            if i == 0:
+                for index, choice in enumerate(q_a[question]["a"]):
+                    answer = int(input(choice))
+                    if index == 0 and answer == 0:
+                        self.user_want[q_a[question]["key"][index]] = 0
+                    elif index == 1 and answer == 1:
+                        self.user_want[q_a[question]["key"][index]] = 50000000
+                    else:
+                        self.user_want[q_a[question]["key"][index]] = answer
+            # Else get only 1 input
+            else:
+                for index, answer in enumerate(q_a[question]["a"]):
+                    print(f"{index+1}. {answer}\n")
+                answer = input("Please enter your choice: ")
+                if i == 6:
+                    self.user_want[q_a[question]["key"]
+                                   ] = q_a[question]["a"][int(answer)-1]
+                else:
+                    answers = answer.split(",")
+                    for answer in answers:
+                        self.user_want[q_a[question]["key"]].append(
+                            q_a[question]["a"][int(answer)-1])
+            question_number += 1
+
+    def get_best_match(self):
+        return db.get_matching_car(self.user_want)
+
+    def get_close_match(self, best_match):
+        return db.get_close_car(best_match, self.user_want)
+
+    def __init__(self, user_want) -> None:
+        self.user_want = user_want
+        self.best_match = []
+        self.close_match = []
+        # self.ask_question()
+
+# For testing
+
+
+def test():
+    car = IntelligenceSystemCar()
+    best_match = car.get_best_match()
+    close_match = car.get_close_match(best_match)
+
+    cwd = os.getcwd()
+    file1 = open(cwd + "/Car_API/Test_Result_File/test_best_matching_car.txt", "w")
+    file2 = open(cwd + "/Car_API/Test_Result_File/test_close_matching_car.txt", "w")
+
+    file1.write(
+        f"We found total of {len(best_match)} best matching car for you!\n")
+    for index, item in enumerate(best_match):
+        file1.write(f"Match no. {index + 1}\n")
+        file1.write(str(json.dumps(item, indent=4))+'\n')
+
+    file2.write(
+        f"We found total of {len(close_match)} close matching car for you!\n")
+    for index, item in enumerate(close_match):
+        file2.write(f"Match no. {index + 1}\n")
+        file2.write(str(json.dumps(item, indent=4))+'\n')
+
+    file1.close()
+    file2.close()
+
+# test()
---
+++ WebApp/views.py
@@ -0,0 +1,130 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from Car_API import api
+import json
+
+views = Blueprint('views', __name__)
+
+user_want = {
+        "min_price": 1000000,
+        "max_price": 50000000,
+        "body_type": ["Any"],
+        "fuel_type": ["Any"],
+        "transmission_type": ["Any"],
+        "color": ["Any"],
+        "brand": ["Any"],
+        "minimum_year": 2015,
+        "vehicle_size": ["Any"],
+        "profile": ["Any"]
+}
+
+@views.route('/')
+def home():
+    return render_template("/index.html")
+
+@views.route('/price', methods=["POST", "GET"])
+def price():
+    return render_template("/html/minmaxprice.html")
+
+@views.route('/type', methods=["POST", "GET"])
+def type():
+    if request.method == 'POST':
+        min_price = request.form.get('minprice')
+        max_price = request.form.get('maxprice')
+        if min_price != "0":
+            user_want["min_price"] = int(min_price)
+        if max_price != "0":
+            user_want["max_price"] = int(max_price)
+    return render_template("/html/bodytype.html")
+
+@views.route('/fuel', methods=["POST", "GET"])
+def fuel():
+    if request.method == 'POST':
+        body_types = request.form.getlist('input_body')
+        if body_types != []:
+            if "Any" not in body_types:
+                user_want["body_type"] = [x for x in body_types]
+        else:
+            user_want["body_type"] = ["Any"]
+    return render_template("/html/fueltype.html")
+
+@views.route('/transmission', methods=["POST", "GET"])
+def transmission():
+    if request.method == 'POST':
+        fuel_types = request.form.getlist('input_fuel')
+        if fuel_types != []:
+            if "Any" not in fuel_types:
+                user_want["fuel_type"] = [x for x in fuel_types]
+        else:
+            user_want["fuel_type"] = ["Any"]
+    return render_template("/html/transmissiontype.html")
+
+@views.route('/brand', methods=["POST", "GET"])
+def brand():
+    if request.method == 'POST':
+        transmissions = request.form.getlist('input_transmission')
+        if transmissions != []:
+            if "Any" not in transmissions:
+                user_want["transmission_type"] = [x for x in transmissions]
+        else:
+            user_want["transmission_type"] = ["Any"]
+    return render_template("/html/brand.html")
+
+@views.route('/color', methods=["POST", "GET"])
+def color():
+    if request.method == 'POST':
+        brands = request.form.getlist('input_brand')
+        if brands != []:
+            if "Any" not in brands:
+                user_want["brand"] = [x for x in brands]
+        else:
+            user_want["brand"] = ["Any"]
+    return render_template("/html/colortype.html")
+
+
+@views.route('/year', methods=["POST", "GET"])
+def year():
+    if request.method == 'POST':
+        colors = request.form.getlist('input_color')
+        if colors != []:
+            if "Any" not in colors:
+                user_want["color"] = [x for x in colors]
+        else:
+            user_want["color"] = ["Any"]
+    return render_template("/html/minimumyear.html")
+
+@views.route('/size', methods=["POST", "GET"])
+def size():
+    if request.method == 'POST':
+        year = request.form.get('input_year')
+        if year != None:
+            if year != "Any":
+                user_want["minimum_year"] = int(year)
+    return render_template("/html/vehiclesize.html")
+
+@views.route('/profile', methods=["POST", "GET"])
+def profile():
+    if request.method == 'POST':
+        sizes = request.form.getlist('input_size')
+        if sizes != []:
+            if "Any" not in sizes:
+                user_want["vehicle_size"] = [x for x in sizes]
+        else:
+            user_want["vehicle_size"] = ["Any"]
+    return render_template("/html/carprofile.html")
+
+@views.route('/result', methods=["POST", "GET"])
+def result():
+    if request.method == 'POST':
+        profiles = request.form.getlist('input_profile')
+        if profiles != []:
+            if "Any" not in profiles:
+                user_want["profile"] = [x for x in profiles]
+        else:
+            user_want["profile"] = ["Any"]
+        car = api.IntelligenceSystemCar(user_want)
+        best_match = car.get_best_match()
+        close_match = car.get_close_match(best_match)
+    if best_match == [] and close_match == []:
+        return render_template("/html/notfound.html")
+    return render_template("/html/result.html", data1=best_match, data2=close_match)
+
